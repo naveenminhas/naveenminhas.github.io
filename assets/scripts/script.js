f538456c71a7f58897d119c10494eca0b60343d5
naveen <avuminhas05@gmail.com>
2021-01-07 12:18:22 +0530

    initial commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+new TypeIt(".multipleStrings", {
+  strings: ["<div class='message one'><i>Hi !!</i></div>",
+  			 "<div class='message two'>I am <b> Naveen Minhas </b>.</div>",
+  			 	"<div class='message three'> Welcome to my portfolio. </div>"],
+  speed: 50,
+  waitUntilVisible: true
+}).go();
